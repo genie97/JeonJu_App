c227b6de0af9378bc91d38518d7a78605c88202f
YEJI LEE <yagee97@naver.com>
2018-11-04 14:44:34 +0900

    yeji+hyeyeon
    yeji+hyeyon

--- JeonjuRo2018/app/src/main/java/org/androidtown/jeonjuro2018/ScheduleActivity.java
+++ JeonjuRo2018/app/src/main/java/org/androidtown/jeonjuro2018/ScheduleActivity.java
@@ -1,6 +1,7 @@
 package org.androidtown.jeonjuro2018;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -17,8 +18,6 @@ import com.kakao.kakaolink.KakaoTalkLinkMessageBuilder;
 
 
 public class ScheduleActivity extends AppCompatActivity implements View.OnClickListener{
-
-    private static final String CAPTURE_PATH = "/CAPTURE";
 
     ImageButton bus_btn;
     ImageButton walk_btn;
